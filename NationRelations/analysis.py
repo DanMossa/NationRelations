e6f179bbc3cb8ec7857f70bb79ba457d320b1953
Erick Velez <erickvelez7@gmail.com>
2019-03-02 19:13:33 -0800

    Update analysis.py

--- NationRelations/analysis.py
+++ NationRelations/analysis.py
@@ -21,7 +21,7 @@ class Analysis:
 	#RETURNS: the overall Magnitude of textToAnalyze
 	def getSentimentMagnitude(self, textToAnalyze):
 		analysisResponse = self.__analyze(textToAnalyze)
-		documentSentiment = analy__sisResponse.document_sentiment
+		documentSentiment = analysisResponse.document_sentiment
 		return documentSentiment.magntiude
 
 
