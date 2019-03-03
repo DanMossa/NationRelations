338bd94c9e581bf79b038ab4f523b7cc10f56ac7
Erick <erickvelez7@gmail.com>
2019-03-02 19:44:17 -0800

    Push to split with Stellan

--- NationRelations/pipeline.py
+++ NationRelations/pipeline.py
@@ -8,5 +8,17 @@ class Pipeline:
     def __init__(self):
         self.db = pymysql.connect("localhost","daniel","!nation!","countrydb" )
         self.analyzer = Analyzer()
+        self.countries_list = get_all_directed_pairs()
 
-    def _queryNews(self, Countries: ):
+    def _insertToTable(self);
+
+
+    def updateDirectedTables(self):
+        for countryPair in self.countries_list:
+            fromCountry = countryPair[0]
+            toCountry = countryPair[1]
+            headlines = get_relevant_headlines(fromCountry, toCountry)
+
+            for headline in headlines:
+                text = headline[0]
+                date = headline[1]
