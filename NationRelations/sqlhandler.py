46702be92ca7be1178ad4a35c39e2ac98fefbc54
Stellan Muller-Cohn <stellan125@gmail.com>
2019-03-03 00:07:00 -0800

    Fixed sql statements, probably

--- NationRelations/sqlhandler.py
+++ NationRelations/sqlhandler.py
@@ -6,13 +6,13 @@ from countries import Countries
 class _SqlHandler:
     _agg_table_name = "aggregate"
 
-    _sql_check_table_template = "SELECT * FROM countrydb.tables WHERE table_name = {table_name}"
-    _sql_insert_template = "INSERT INTO {table_name} VALUES ({text}, {sentiment}, {magnitude}, {date})"
+    """    _sql_check_table_template = "SELECT * FROM countrydb.tables WHERE table_name = {table_name}"
+    _sql_insert_template = "INSERT INTO {table_name} (`TEXT`,`SENTIMENT`,`MAGNITUDE`,`DATE`) VALUES ({text}, {sentiment}, {magnitude}, {date})"
     _sql_get_range_template = "SELECT * FROM '{table_name}' LIMIT {start_index}, {row_count}"
     _sql_get_template = "SELECT * FROM '{table_name}' WHERE ROWNUM={index}"
     _sql_set_aggregate_template = "UPDATE " + _agg_table_name + " SET 'TITLE_SENTEMENT'={new_val} WHERE 'FROM' = '{home_country}' AND 'TO' = {away_country}"
     _sql_get_aggregate_template = "SELECT * FROM " + _agg_table_name + " WHERE 'FROM' = '{home_country}' AND 'TO' = {away_country}"
-
+    """
     def __init__(self):
         self._db = pymysql.connect("localhost", "daniel", "!nation!", "countrydb")
         self._cursor = self._db.cursor()
@@ -22,7 +22,8 @@ class _SqlHandler:
 
     def table_exists(self, table_name):
         """Checks if a table is present in the database"""
-        statement = self._sql_check_table_template.format(table_name=table_name)
+        statement = "SHOW TABLES LIKE " + table_name
+
         self._cursor.execute(statement)
         result = self._cursor.fetchall()
         if len(result) > 0:
@@ -31,7 +32,7 @@ class _SqlHandler:
 
     def store_sentiment_data(self, home_country: Countries, away_country: Countries, text, sentiment, magnitude, date):
         tname = self._get_directed_table_name(home_country, away_country)
-        statement = self._sql_insert_template.format(table_name=tname, text=text, sentiment=sentiment, magnitude=magnitude, date=date)
+        statement = "INSERT INTO " + tname + " (`text`,`sentiment`,`magnitude`,`date`) VALUES (\"" + text + "\", " + sentiment + ", " + magnitude + "\"" + date + "\")"
 
         self._cursor.execute(statement)
         self._db.commit()
@@ -52,7 +53,7 @@ class _SqlHandler:
         if isinstance(row, tuple):
             start = min(row)
             rows = max(row) - start
-            statement = self._sql_get_range_template.format(table_name=tname, start_index=start, row_count=rows)
+            statement = "SELECT * FROM " + tname + " LIMIT " + start + "," + rows
 
             self._cursor.execute(statement)
             results_raw = self._cursor.fetchall()
@@ -62,7 +63,7 @@ class _SqlHandler:
                 results.append(d)
             return results
         else:
-            statement = self._sql_get_template.format(table_name=tname, index=row)
+            statement = "SELECT * FROM " + tname + " WHERE ROWNUM=" + row
 
             self._cursor.execute(statement)
             results_raw = self._cursor.fetchall()
@@ -74,16 +75,17 @@ class _SqlHandler:
 
     def set_aggregate_val(self, home_country: Countries, away_country: Countries, new_val):
         """Sets the aggregate value for the specified directed country pair"""
-        statement = self._sql_set_aggregate_template.format(new_val=new_val, home_country=home_country.get_iso_code(), away_country=away_country.get_iso_code())
+        statement = "UPDATE " + self._agg_table_name + " SET title_sentiment=" + new_val + " WHERE from=" + home_country.get_iso_code() + " AND to=" + away_country.get_iso_code()
 
         self._cursor.execute(statement)
         self._db.commit()
 
     def get_aggregate_val(self, home_country: Countries, away_country: Countries):
-        statement = self._sql_get_aggregate_template.format(home_country=home_country.get_iso_code(), away_country=away_country.get_iso_code())
+        statement = "SELECT * FROM " + self._agg_table_name + " WHERE from=" + home_country.get_iso_code() + " AND to=" + away_country.get_iso_code()
 
         self._cursor.execute(statement)
         result = self._cursor.fetchall()
         if len(result) == 1:
             return result[0]
         return None
+
