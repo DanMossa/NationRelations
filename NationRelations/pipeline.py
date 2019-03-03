c58960b5c496982670e0f18737bd92e4d702ed22
Erick <erickvelez7@gmail.com>
2019-03-02 20:55:03 -0800

    Break pipeline sentiment function into multiple

--- NationRelations/pipeline.py
+++ NationRelations/pipeline.py
@@ -10,15 +10,33 @@ class Pipeline:
         self.analyzer = Analyzer()
         self.countries_list = get_all_directed_pairs()
 
-    def updateDirectedTables(self):
-        for countryPair in self.countries_list:
-            fromCountry = countryPair[0]
-            toCountry = countryPair[1]
-            headlines = get_relevant_headlines(fromCountry, toCountry)
+    #RETURNS: tuple of format (SCORE, MAGNITUDE)
+    def _analyze_headline(self, headline_text):
+        return self.analyzer.getSentiment(headline_text)
 
-            for headline in headlines:
-                text = headline[0]
-                date = headline[1]
+    #RETURNS: a list of tuples with format (TEXT, SCORE, MAGNITUDE, DATE)
+    #which resembles the records to be stored in the directed SQL tables
+    def _compile_directed_country_records(self, headlines_list):
+        records_list = list()
 
-                sentimentTuple = self.analyzer.getSentiment(text)
-                recordTuple = (text, sentimentTuple[0], sentimentTuple[1], date)
+        for headline in headlines_list):
+            text = headline[0]
+            date = headline[1]
+
+            sentiment_tuple = self.analyze_headline(text)
+            score = sentiment_tuple[0]
+            magnitude = sentiment_tuple[1]
+
+            records_list.append((text, score, magnitude, date))
+        return records_list
+
+    def _get_directed_pair_headlines(self, from_country, to_country):
+        return get_relevant_headlines(from_country, to_country)
+
+    def compile_all_sentiment_values(self):
+        for country_pair in self.countries_list:
+            from_country = country_pair[0]
+            to_country = country_pair[1]
+
+            headlines_list = self._get_directed_pair_headlines(from_country, to_country)
+            records_to_add = self._compile_directed_country_records(headlines_list)
