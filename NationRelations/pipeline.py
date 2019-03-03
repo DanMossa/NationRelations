a72bdf6f17a0634564abec34a14b5c1b4f246979
Erick <erickvelez7@gmail.com>
2019-03-02 21:43:38 -0800

    Write directed records to database

--- NationRelations/pipeline.py
+++ NationRelations/pipeline.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 from countries import get_country_name
 from countries import Countries
-from analyzer import Analyzer
 from sqlhandler import _SqlHandler
 
 class Pipeline:
     def __init__(self):
-        self.db_handler = _SqlHandler()
+        self.database = _SqlHandler()
         self.analyzer = Analyzer()
         #List of pairs of Country enums
         self.countries_list = get_all_directed_pairs()
@@ -17,7 +16,7 @@ class Pipeline:
 
     #RETURNS: a list of tuples with format (TEXT, SCORE, MAGNITUDE, DATE)
     #which resembles the records to be stored in the directed SQL tables
-    def _compile_directed_country_records(self, headlines_list):
+    def __generate_directed_country_records(self, headlines_list):
         records_list = list()
 
         for headline in headlines_list:
@@ -31,13 +30,21 @@ class Pipeline:
             records_list.append((text, score, magnitude, date))
         return records_list
 
-    def _get_directed_pair_headlines(self, from_country, to_country):
+    def _get_directed_pair_headlines(self, from_country: Countries, to_country: Countries):
         return get_relevant_headlines(from_country, to_country)
 
-    def compile_all_sentiment_values(self):
+    def _write_to_directed_table(self, from_country: Countries, to_country: Countries, record_tuple):
+        self.database.store_sentiment_data(from_country, to_country, *record_tuple)
+
+    #Takes the list of directed country pairs, retrieves the related headlines,
+    #creates record-format tuples, then writes them to the database
+    def process_directed_country_data(self):
         for country_pair in self.countries_list:
             from_country = country_pair[0]
             to_country = country_pair[1]
 
             headlines_list = self._get_directed_pair_headlines(from_country, to_country)
             records_to_add = self._compile_directed_country_records(headlines_list)
+
+            for record in records_to_add:
+                self._write_to_directed_table(from_country, to_country, record)
