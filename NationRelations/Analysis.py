a4810fc560a77fb79b8100f1a0e86f23c29deff4
Erick <erickvelez7@gmail.com>
2019-03-02 16:20:44 -0800

    Make analyze private

--- NationRelations/Analysis.py
+++ NationRelations/Analysis.py
@@ -6,33 +6,33 @@ class Analysis:
 	#RETURNS: JSON object containing overall sentiment and an array of sentences
 	#with individual sentences
 	def __analyze(self, textToAnalyze):
-		#put textToAnalyze into a JSON object
+		#put textToAnalyze in Document format
 		document = {'type': self.documentType_, 'content': textToAnalyze}
-		#analyze_sentiment returns the JSON object with sentiment scores in them
-		analysisResponse = self.client.analyze_sentiment(document)
+		#analyze_sentiment returns 'document' with sentiment scores in them
+		analysisResponse = self.language_client.analyze_sentiment(document)
 		return analysisResponse
 
 	#RETURNS: the overall Score of textToAnalyze
 	def getSentimentScore(self, textToAnalyze):
-		analysisResponse = self.analyze(textToAnalyze)
+		analysisResponse = self.__analyze(textToAnalyze)
 		documentSentiment =  analysisResponse.document_sentiment
 		return documentSentiment.score
 
 	#RETURNS: the overall Magnitude of textToAnalyze
 	def getSentimentMagnitude(self, textToAnalyze):
-		analysisResponse = self.analyze(textToAnalyze)
-		documentSentiment = analysisResponse.document_sentiment
+		analysisResponse = self.__analyze(textToAnalyze)
+		documentSentiment = analy__sisResponse.document_sentiment
 		return documentSentiment.magntiude
 
 
 	#RETURNS: a tuple with format (SCORE, MAGNITUDE) of the overall textToAnalyze
 	def getSentiment(self, textToAnalyze):
-		analysisResponse = self.analyze(textToAnalyze)
+		analysisResponse = self.__analyze(textToAnalyze)
 		documentSentiment = analysisResponse.document_sentiment
 		score = documentSentiment.score
 		magnitude = documentSentiment.magnitude
 		return (score, magnitude)
 
 	if __name__ == '__main__':
-		self.client = language.LanguageServiceCliet()
+		self.language_language_client = language.LanguageServiceCliet()
 		self.documentType_ = enums.Document.Type.PLAIN_TEXT
