4eb85255423bf0004fe54a3cd9e4fd1a275aad71
Daniel Mossaband <Daniel@Mossaband.com>
2019-03-02 17:28:51 -0800

    Add basic routing

--- NationRelations/__init__.py
+++ NationRelations/__init__.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello():
-	return "Hello!!"
+    return render_template("home.html")
+
+@app.route("/daniel")
+def daniel():
+    return "Daniel loves flask with all his heart"
+
 if __name__ == "__main__":
-	app.run()
+    app.run(debug=True)
